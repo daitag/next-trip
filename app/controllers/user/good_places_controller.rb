e99d85d0e1ba7dd37dae96f56ef3785cadca81d1
daitag <[dik.sin118@gmail.com]>
2020-08-06 06:00:09 +0000

    [add]good_place_ajax

--- app/controllers/user/good_places_controller.rb
+++ app/controllers/user/good_places_controller.rb
@@ -3,13 +3,11 @@ class User::GoodPlacesController < ApplicationController
 		@post = Post.find(params[:post_id])
 		good_place = current_user.good_places.new(post_id: @post.id)
 		good_place.save
-		redirect_to request.referer
 	end
 
 	def destroy
 		@post = Post.find(params[:post_id])
 		good_place = current_user.good_places.find_by(post_id: @post.id)
 		good_place.destroy
-		redirect_to request.referer
 	end
 end
