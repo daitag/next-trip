5cacc02862a38eeb882cb07fe1169bde7c7a4a20
daitag <dik.sin118@gmail.com>
2020-08-28 11:32:54 +0000

    [fix]admin_login

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create!( admin_name: "administrator",
+Admin.create!(
 			   email: "admin@gmail.com",
 			   password: "password")
 
